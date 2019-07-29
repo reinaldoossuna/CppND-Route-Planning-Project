9bc059b463afa62799975b8a12e03b996798af3c
Reinaldo Ossuna <reinaldo.ossuna@gmail.com>
2019-07-29 14:28:12 -0400

    Extend the classes from IO2D

--- src/route_model.cpp
+++ src/route_model.cpp
@@ -3,4 +3,76 @@
 
 RouteModel::RouteModel(const std::vector<std::byte> &xml) : Model(xml) {
 
+  int counter =0;
+  for(Model::Node node : this->Nodes()){
+    m_Nodes.push_back(Node(counter,this,node));
+    counter++;
+  }
+  CreateNodeToRoadHashmap();
+}
+
+void RouteModel::CreateNodeToRoadHashmap(){
+
+  // Loop through the roads
+  for (const Model::Road &road : this->Roads() ) {
+    // check if the road is not a footway
+    if (road.type != Model::Road::Type::Footway ) {
+      // loop through all nodes in way
+      for (int node_idx : this->Ways()[road.way].nodes) {
+        // check if the noad is in the hashmap, if not create a empyt vector
+        if (node_to_road.find(node_idx) == node_to_road.end())
+          node_to_road[node_idx] = std::vector<const Model::Road *> ();
+
+        // push back pointer to the road in the hashmap
+        node_to_road[node_idx].push_back(&road);
+      }
+    }
+  }
+}
+
+RouteModel::Node* RouteModel::Node::FindNeighbor(std::vector<int> node_indices){
+  Node *closest_node = nullptr;
+  Node inTest;
+
+  for (int idx: node_indices) {
+    inTest = parent_model->SNodes().at(idx);
+    if (!inTest.visited && this->distance(inTest) != 0) {
+      if (closest_node == nullptr || this->distance(inTest) < this->distance(*closest_node)){  
+        closest_node = &(parent_model->SNodes().at(idx) );
+      }
+    }
+  }
+  return closest_node;
+}
+
+void RouteModel::Node::FindNeighbors() {
+  for (auto & road: parent_model->node_to_road[this->index]) {
+    RouteModel::Node* new_neighbor = this->FindNeighbor(parent_model->Ways()[road->way].nodes);
+    if (new_neighbor)
+      (this->neighbors).emplace_back(new_neighbor);
+  }
+}
+
+RouteModel::Node& RouteModel::FindClosestNode(float x,float y) {
+  Node input;
+  input.x = x;
+  input.y = y;
+
+  Node inTest;
+
+  float min_dist = std::numeric_limits<float>::max();
+  int closest_idx;
+
+  for (const Model::Road road: this->Roads()) {
+    if (road.type != Model::Road::Type::Footway) {
+      for (int node_idx: this->Ways()[road.way].nodes) {
+        inTest = this->SNodes().at(node_idx);
+        if (inTest.distance(input) < min_dist) {
+          min_dist = inTest.distance(input);
+          closest_idx = node_idx;
+        }
+      }
+    }
+  }
+  return this->SNodes().at(closest_idx);
 }
--- src/route_model.h
+++ src/route_model.h
@@ -4,6 +4,7 @@
 #include <cmath>
 #include <unordered_map>
 #include "model.h"
+#include <unordered_map>
 #include <iostream>
 
 class RouteModel : public Model {
@@ -15,18 +16,35 @@ class RouteModel : public Model {
         
         Node(){}
         Node(int idx, RouteModel * search_model, Model::Node node) : Model::Node(node), parent_model(search_model), index(idx) {}
+        Node *parent = nullptr;
+        float h_value = std::numeric_limits<float>::max();
+        float g_value = 0.0;
+        bool visited = false;
+        std::vector<Node *> neighbors;
+        float distance(Node other) const{
+          return std::sqrt( std::pow((x -other.x),2) + std::pow((y - other.y),2));
+        }
+        void FindNeighbors();
       
       private:
         // Add private Node variables and methods here.
         int index;
         RouteModel * parent_model = nullptr;
+        Node* FindNeighbor(std::vector<int> node_indices);
+
     };
     
     // Add public RouteModel variables and methods here.
     RouteModel(const std::vector<std::byte> &xml);  
     std::vector<Node> path; // This variable will eventually store the path that is found by the A* search.
 
+    auto &SNodes() {return m_Nodes;}
+    auto &GetNodeToRoadMap() {return node_to_road;}
+    Node &FindClosestNode(float x, float y);
+
   private:
     // Add private RouteModel variables and methods here.
-
+    std::vector<Node> m_Nodes = {};
+    std::unordered_map<int,std::vector<const Model::Road*>> node_to_road;
+    void CreateNodeToRoadHashmap();
 };
